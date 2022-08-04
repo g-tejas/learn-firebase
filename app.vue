d5f0281dcfeb8c98821cc580011db1c48d4232b1
Tejas G <ttejasgarrepally@gmail.com>
2022-08-04 21:32:13 +0800

    added vuetify and firebase auth functions

--- app.vue
+++ app.vue
@@ -1,5 +1,27 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <v-btn @click="signIn">Sign In</v-btn>
+    <v-btn @click="signOut">Sign Out</v-btn>
   </div>
 </template>
+
+<script setup>
+
+const signIn = async () => {
+  const email = "tejas@gmail.com";
+  const password = "123456";
+  const credentials = await signInUser(email, password);
+  console.log("Credentials: ", credentials)
+}
+
+const signOut = async () => {
+  const email = "tejas@gmail.com";
+  const password = "123456";
+  const credentials = await signOutUser();
+  console.log("Credentials: ", credentials) // this will always show undefined because user is not signed i
+}
+
+onMounted(async () => {
+  console.log('mounted');
+});
+</script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,15 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+    css: ['vuetify/lib/styles/main.sass', 'mdi/css/materialdesignicons.min.css'],
+    build: {
+        transpile: ['vuetify'],
+    },
+    vite: {
+        define: {
+            'process.env.DEBUG': false,
+        },
+    },
     runtimeConfig: {
         // The private keys which are only available within server-side
         FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
