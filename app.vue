97a2638de2f5500e9218d12e1c5aab744f2b0bab
Tejas G <ttejasgarrepally@gmail.com>
2022-08-04 21:40:56 +0800

    display credentials when logged in

--- app.vue
+++ app.vue
@@ -2,22 +2,26 @@
   <div>
     <v-btn @click="signIn">Sign In</v-btn>
     <v-btn @click="signOut">Sign Out</v-btn>
+    <pre>
+      {{ credentials }}
+    </pre>
   </div>
 </template>
 
 <script setup>
+const credentials = ref();
 
 const signIn = async () => {
   const email = "tejas@gmail.com";
   const password = "123456";
-  const credentials = await signInUser(email, password);
+  credentials.value = await signInUser(email, password);
   console.log("Credentials: ", credentials)
 }
 
 const signOut = async () => {
   const email = "tejas@gmail.com";
   const password = "123456";
-  const credentials = await signOutUser();
+  credentials.value = await signOutUser();
   console.log("Credentials: ", credentials) // this will always show undefined because user is not signed i
 }
 
